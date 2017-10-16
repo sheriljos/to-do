836f1aaef3ebd86219489d6f955d99eda6a07e27
sheriljos <sherilgreg@gmail.com>
2017-10-16 18:06:09 +0200

    changing app.js to index.js

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ todoController(app);
 
 
 //listen to a port
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', http.address().port);
+app.listen(process.env.PORT || 3000, function(){
+console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 
